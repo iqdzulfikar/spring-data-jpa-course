73ef3334e6b003c195854e6a5985efcc5a919acc
Rifqi Dzulfikar <rifqidzulfikar2000@gmail.com>
2022-12-23 16:27:05 +0700

    done

--- src/main/java/rifqidzulfikar/Application.java
+++ src/main/java/rifqidzulfikar/Application.java
@@ -1,7 +1,11 @@
 package rifqidzulfikar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import rifqidzulfikar.model.Student;
+import rifqidzulfikar.repository.StudentRepository;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,17 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//		return args -> {
+//			Student rifqi = new Student(
+//					"Rifqi",
+//					"Dzulfikar",
+//					"rifqidzulfikar2000@gmail.com",
+//					22);
+//			studentRepository.save(rifqi);
+//		};
+//	}
 
 }
